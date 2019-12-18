e01a9952b81487a50cb226250be3ed20d4da036b
ASEDOS999 <kuruzov2014@mail.ru>
2019-12-18 20:37:00 +0300

    Cognitive Assistant is created

--- CognitiveAssistent/CA_Examples/trivial/trivial.py
+++ CognitiveAssistent/CA_Examples/trivial/trivial.py
@@ -10,7 +10,8 @@ from CA_create import ScriptGraphVert as SCV
 dict_vert = {}
 
 vert = SCV(name = "start", 
-        texts = ["Здравствуйте. Приступим к выбору автомобиля."])
+        texts = ["Здравствуйте. Приступим к выбору автомобиля."],
+        is_start = True)
 dict_vert["start"] = vert
 
 name = "Выбор"
@@ -23,7 +24,7 @@ dict_vert[name] = vert
 name = "Осмотр"
 vert = SCV(name = name,
         texts = [])
-dict_vert[name] = name
+dict_vert[name] = vert
 
 name = "Осмотр_бу"
 vert = SCV(name = name,
@@ -32,7 +33,7 @@ vert = SCV(name = name,
             "Прокатитесь на автомобиле."])
 dict_vert[name] = vert
 
-vert = "Осмотр_новая"
+name = "Осмотр_новая"
 vert = SCV(name = name,
         texts = ["Осмотрите представленные в салоне автомобиле. Оцените комфорт и соответствие желаниям",
             "Побеседуйте с консультантом. Распросите про возможные варианты, скидки и бонусы.",
@@ -45,10 +46,10 @@ vert = SCV(name = name,
         texts = ["Спросите у продавца ВИН номер автомоболи и проверьте данные автомобиля на сайте ГИБДД."])
 dict_vert[name] = vert
 
-name = "Оформление покупки"
+name = "Оформление_покупки"
 vert = SCV(name = name,
         texts = [])
-dict_vert[name] = name
+dict_vert[name] = vert
 
 name = "Оформление_бу_частник"
 vert = SCV(name = name,
@@ -57,7 +58,7 @@ vert = SCV(name = name,
             "Получите ключи от Авто.",
             "Оформите ОСАГО.",
             "Зарегестируйте авто в ГИБДД."])
-dict_vert[name] = name
+dict_vert[name] = vert
 
 name = "Оформление_новая_БезКредита"
 vert = SCV(name = name,
@@ -67,7 +68,7 @@ vert = SCV(name = name,
             "Внесите полную стоимость авто и получите его.",
             "Оформите ОСАГО. Подумайте над КАСКО. ...Советы, различия...",
             "Зарегистрируйте авто в ГИБДД"])
-dict_vert[name] = name
+dict_vert[name] = vert
 
 name  = "Оформление_новая_кредит"
 vert = SCV(name = name,
@@ -77,6 +78,27 @@ vert = SCV(name = name,
             "Получите авто.",
             "Зарегистрируйте авто в ГИБДД",
             "Передайте ПТС банку, выдавшему кредит"])
-dict_vert[name] = name
+dict_vert[name] = vert
 
-dict_vert["end"] = SCV(name = "end", texts = ["Поздравляю с покупкой автомобиля."])
+dict_vert["end"] = SCV(name = "end", texts = ["Поздравляю с покупкой автомобиля."], is_terminal = True)
+
+dict_vert["start"].add_edge(child = dict_vert["Выбор"], cond = "")
+dict_vert['start'].add_edge(child = dict_vert["Осмотр"], cond = "Осмотр")
+dict_vert["start"].add_edge(child = dict_vert["Оформление_покупки"], cond = "Оформление")
+dict_vert['Выбор'].add_edge(child = dict_vert["Осмотр"], cond = "")
+dict_vert["Осмотр"].add_edge(child = dict_vert["Осмотр_бу"], cond = "бу")
+dict_vert["Осмотр"].add_edge(child = dict_vert["Осмотр_новая"], cond = "новая")
+dict_vert["Осмотр_бу"].add_edge(child = dict_vert["Оформление_покупки"], cond = "")
+dict_vert["Осмотр_новая"].add_edge(child = dict_vert["Оформление_покупки"], cond = "")
+dict_vert["Оформление_покупки"].add_edge(child = dict_vert["Оформление_бу_частник"], cond = "бу_частник")
+dict_vert["Оформление_покупки"].add_edge(child = dict_vert["Оформление_новая_БезКредита"], cond = "новая без кредита")
+dict_vert["Оформление_покупки"].add_edge(child = dict_vert["Оформление_новая_кредит"], cond = "новая с кредитом")
+dict_vert["Оформление_бу_частник"].add_edge(child = dict_vert["end"], cond = "")
+dict_vert["Оформление_новая_БезКредита"].add_edge(child = dict_vert["end"], cond = "")
+dict_vert["Оформление_новая_кредит"].add_edge(child = dict_vert["end"], cond = "")
+
+CA = CognitiveAssistant("trivial", dict_vert["start"])
+CA.dialogue_start()
+with open("CA_trivial.pickle", "wb") as f:
+    pickle.dump(CA, f)
+    f.close()
